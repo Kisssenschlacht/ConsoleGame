7f6cb42f6b2c12e7fbb52d9074047ebe61c6c4ea
frndotmsn <frndotmsn@jofrix.com>
2022-12-16 19:44:52 +0100

    removed entitieshandler file

--- src/program/Program.cs
+++ src/program/Program.cs
@@ -42,7 +42,22 @@ namespace ConsoleGame
         private void SpawnRandomEntity(TimeSpan elapsedTime)
         {
             if (elapsedTime.Milliseconds != 0 && RandomNumberGenerator.GetInt32(10 * 1000 / elapsedTime.Milliseconds) == 0 && _state.Map.Entities.Count() < Constants.EntityLimit)
-                SpawnRandomEntity();
+            {
+                Position[] possiblePositionsArray =
+                Enumerable.Range(0, Constants.width * Constants.height)
+                .Select(z => new Position() { x = z % Constants.width, y = z / Constants.width })
+                .Where(x => !_state.Map.Entities.Select(y => y.Position).Contains(x)).ToArray();
+                var chosenPosition = possiblePositionsArray.Random();
+                if (chosenPosition == null) return;
+                Position nonnullChosenPosition = (Position)chosenPosition;
+                Func<Entity>? constructor = new Func<Entity>[] {
+                () => new Entities.Cow(_state.Map, nonnullChosenPosition),
+                () => new Entities.Sheep(_state.Map, nonnullChosenPosition)
+            }.Random();
+                Entity entity;
+                if (constructor == null || (entity = constructor()) == null) return;
+                _state.Map.Entities.Add(entity);
+            }
         }
         // returns whether the game loop should stop
     }
